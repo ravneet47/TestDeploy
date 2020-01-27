8fe9e0d6ea0191eb2245af1aa281e7af8906572c
Ravneet Saggu <46670367+ravneet47@users.noreply.github.com>
2020-01-27 15:02:08 +0530

    Create pre-requisites.sh

---
+++ pre-requisites.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#------------------Installing Jenkins-------------------
+
+sudo yum update
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+sudo yum install jenkins
+sudo service jenkins start
+sudo chkconfig jenkins on
+
+#------------------------------------------------------
